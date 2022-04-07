37d5eae8478cdcb38de843740743c2de2c198a64
weiwuhao <weiwuhao@blackjack-inc.com>
2022-04-07 19:43:09 +0800

    refactor code

--- UnityProject/Assets/TinyRenderer/TinyRenderer.cs
+++ UnityProject/Assets/TinyRenderer/TinyRenderer.cs
@@ -6,21 +6,35 @@ using UnityEngine.UI;
 public class TinyRenderer : MonoBehaviour
 {
     
+    void Render()
+    {
+        SetColor(50,50,Color.green);
+    }
+
+    void SetColor(int x,int y,Color color)
+    {
+       this.m_texture.SetPixel(x,y,color); 
+    }
+
+    void DrawLine()
+    {
+
+    }
 
     void Start()
     {
         m_rawImg.texture= new Texture2D(this.m_width, this.m_height, TextureFormat.RGBA32, false);
-        var texture=m_rawImg.texture as Texture2D;
-        texture.SetPixel(52,41,Color.red);
-        texture.Apply();
-
+        m_texture = m_rawImg.texture as Texture2D;
 
     }
 
     void Update()
     {
+        Render();
+        this.m_texture.Apply();
         
     }
+
 
     [SerializeField]
     private int m_width;
@@ -28,4 +42,6 @@ public class TinyRenderer : MonoBehaviour
     private int m_height;
     [SerializeField]
     private RawImage m_rawImg;
+
+    private Texture2D m_texture;
 }
